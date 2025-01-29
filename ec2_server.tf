5f3e33c92d56cbb35d6ef1b2e2068d7c9eab8b07
Pranat Pattankude <pranatpattanakude@gmail.com>
2025-01-29 11:44:56 +0530

    update

--- ec2_server.tf
+++ ec2_server.tf
@@ -1,17 +1,24 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "terra-automate-key"
+  public_key = file("C:/Users/prana/.ssh/id_ed25519.pub")
+}
+
 resource "aws_instance" "AppServer" {
-  ami                         = "ami-0182f373e66f89c85"
+  ami                         = "ami-04b4f1a9cf54c11d0"
   instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.deployer.key_name
   subnet_id                   = aws_subnet.pub-sub.id
   vpc_security_group_ids      = [aws_security_group.security.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo -i
-    sudo yum install httpd -y
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
+    sudo apt update -y
+    sudo apt install apache2 -y
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
     EOF
+
 
   tags = {
     Name = "App Server"
--- output.tf
+++ output.tf
@@ -7,8 +7,3 @@ output "instance_ip_url" {
   description = "Public Url of Instance"
   value = "https://${aws_instance.AppServer.public_ip}"
 }
-
-output "SSH_login" {
-  description = "Login using ssh"
-  value       = "ssh -i <private key> ec2-user@${aws_instance.AppServer.public_ip}"
-}
